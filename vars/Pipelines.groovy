6b0bfcbca720ab5993820d866db0c1abcef5c039
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-04 18:28:24 +0000

    cenas

--- vars/Pipelines.groovy
+++ vars/Pipelines.groovy
@@ -1,38 +1,22 @@
 
-package com.criticalsoftware.automation
 
-//@Library('Pipeline-Global-Library') _
+def call(body) {
+    // evaluate the body block, and collect configuration into the objectdef pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
 
-
-
-
-def call(Map config) {
-
-if (${config.name} == "sometext") {
     pipeline {
-      agent any
-      stages {
-        stage('build') {
-          steps {
-            echo "condition one"
+        // our complete declarative pipeline can go in here
+        agent any
+          stages {
+            stage('build') {
+              steps {
+                echo "condition one"
+             }
+            }
           }
         }
-      }
-    }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('build') {
-          steps {
-            echo "condition two"
-          }
-        }
-      }
-    }
-  }
-
 }
 
-return this
 
