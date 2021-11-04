818297b8a7fd57b378c9114a54ab3b9186274043
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-04 17:55:40 +0000

    cenas

--- src/com/criticalsoftware/automation/Pipelines.groovy
+++ src/com/criticalsoftware/automation/Pipelines.groovy
@@ -3,36 +3,10 @@ package com.criticalsoftware.automation
 
 class Pipelines{
 
-    
+
 def devops_call(String repoName) {
-    println "The repository name inside the parameter is $repoName"
-
-if (repoName == "sometext") {
-    pipeline {
-      agent any
-      stages {
-        stage('build') {
-          steps {
-            echo "condition one"
-          }
-        }
-      }
-    }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('build') {
-          steps {
-            echo "condition two"
-          }
-        }
-      }
-    }
-  }
-
-}
 
 
 }
 
+}
