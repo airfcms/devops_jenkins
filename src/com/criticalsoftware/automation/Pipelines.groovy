9114a24a208a915971961a992588f668a46b5596
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-04 17:56:40 +0000

    cenas

--- src/com/criticalsoftware/automation/Pipelines.groovy
+++ src/com/criticalsoftware/automation/Pipelines.groovy
@@ -5,8 +5,33 @@ class Pipelines{
 
 
 def devops_call(String repoName) {
+    
+if (repoName == "sometext") {
+    pipeline {
+      agent any
+      stages {
+        stage('build') {
+          steps {
+            echo "condition one"
+          }
+        }
+      }
+    }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('build') {
+          steps {
+            echo "condition two"
+          }
+        }
+      }
+    }
+  }
+
+}
 
 
 }
 
-}
