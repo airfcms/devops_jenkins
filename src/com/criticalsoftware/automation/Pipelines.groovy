2e3cea45b31e08f0c86cf92a25e5412011e7058d
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-04 17:57:57 +0000

    cenas

--- src/com/criticalsoftware/automation/Pipelines.groovy
+++ src/com/criticalsoftware/automation/Pipelines.groovy
@@ -1,11 +1,13 @@
 
 package com.criticalsoftware.automation
 
+@Library('Pipeline-Global-Library') _
+
 class Pipelines{
 
 
 def devops_call(String repoName) {
-    
+
 if (repoName == "sometext") {
     pipeline {
       agent any
