9f3e4b954be653734aff491d3cb757eea2bd330e
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-04 17:32:39 +0000

    change groovy file

--- src/com/criticalsoftware/automation/pipelines.groovy
+++ src/com/criticalsoftware/automation/pipelines.groovy
@@ -4,95 +4,32 @@
 
 def devops_call(String repoName) {
     println "The repository name inside the parameter is $repoName"
+
+if (repoName == "sometext") {
+    pipeline {
+      agent any
+      stages {
+        stage('build') {
+          steps {
+            echo "condition one"
+          }
+        }
+      }
+    }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('build') {
+          steps {
+            echo "condition two"
+          }
+        }
+      }
+    }
+  }
+
 }
 
-
-/*
-
-currentBuild.result = "SUCCESS"
-String buildAgentLabel = "" //add your build agent name here (???)
-//noinspection GroovyUnusedAssignment
-@Library('Pipeline-Global-Library')
-
-try {
-    stage("Build") {
-        node(buildAgentLabel) {
-            envSetup()  //(???)
-            build()
-        }
-    }
-/*
-    stage("Unit Testing") {
-        node(buildAgentLabel) {
-            unitTesting()
-        }
-    }
-
-    stage("SW Integration Testing") {
-        node(buildAgentLabel) {
-            functionalTesting()
-        }
-    }
-
-    stage("HW/SW Integration Testing") {
-        node(buildAgentLabel) {
-            performanceTesting()
-        }
-    }
-
-    stage("Promote") {
-        node(buildAgentLabel) {
-            promote()
-        }
-    }
-
-    stage("Deploy") {
-        node(buildAgentLabel) {
-            deploy()
-        }
-    }
-
-    stage("Static Analysis") {
-        node(buildAgentLabel) {
-            staticAnalysis()
-        }
-    }
-
-*/
-/*
-}
-catch (err) {
-    echo err.toString()
-    currentBuild.result = "FAILURE"
-}
-
-def envSetup(){
-  // (??)  deleteDir() //delete jenkins working dir for this project
-    checkout scm //clones the git repository
-}
-
-def build() {
-
-    // Get the image from airfcms docker/repo registry in artifactory
-    // Docker run the image to build the environment (virtualization)
-    // git clone the repositories inside the image (??)
-    // Compile the project inside the cloned repository(??)
-
-}
-/*
-def staticAnalysis() {}
-
-def unitTesting() {}
-
-def deploy() {
-    // Send the artifact to azure.artifactory generic repo (??)
-}
-
-def functionalTesting() {}
-
-def performanceTesting() {}
-
-def promote() {}
- */
 
 
