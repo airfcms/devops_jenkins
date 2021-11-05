410edd57cb0b4a808c2e9a9923f5b628e8d4eebb
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-05 16:51:58 +0000

    change pipeline

--- vars/Pipelines.groovy
+++ vars/Pipelines.groovy
@@ -44,7 +44,7 @@ def call(body) {
                 echo 'Testing docker run from artifactory -- Success'
                 echo sh (script: "ls -lha", returnStdout: true)
                 sh 'printenv'
-                sh 'echo ${scm.getUserRemoteConfig()}'
+                sh 'echo ${scmUrl}'
                 sh 'sudo rm -rf ${WORKSPACE}/'
 
                 sh"""
