171d2d3712e3dc62a2842d987c5f0d3706ceab53
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-05 13:32:19 +0000

    change pipeline

--- vars/Pipelines.groovy
+++ vars/Pipelines.groovy
@@ -1,5 +1,4 @@
-//Question for Várzea. Shouldn't we have a repository to keep the docker images need for each repository, and just call them to build
-//the image, send the image to registry, and then push the image to build the environment, instead of manually upload it to registry?
+
 
 def call(body) {
     // evaluate the body block, and collect configuration into the objectdef
@@ -12,29 +11,17 @@ def call(body) {
     pipeline {
 
         environment{
-          String DOCKER_IMAGE = pipelineParams['image']
-          String DOCKER_REG_ARTIFACTORY = pipelineParams['registryUrl']
-          String DOCKER_REG_ARTIFACTORY_TOKEN = pipelineParams['registryUrl']
+          String DOCKER_IMAGE = pipelineParams['dockerImage']
+          String DOCKER_REG_ARTIFACTORY = pipelineParams['dockerRegistryUrl']
+          String DOCKER_REG_ARTIFACTORY_TOKEN = pipelineParams['dockerRegistryUrl']
+          def scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
         }
 
         agent any
-        /* // Only available in steps block (???)
-          parameters {
-            string(name: 'DOCKER_IMAGE', defaultValue: pipelineParams['image'], description: 'path to image in docker registry')
-            string(name: 'DOCKER_REG_ARTIFACTORY',  defaultValue: pipelineParams['registryUrl'], description: 'path to docker registry')
-            password(name: 'DOCKER REG_ARTIFACTORY_TOKEN', defaultValue: pipelineParams['registryCredentialsId'], description: 'token to registry')
-          }
-          */
           stages {
             stage('build') {
               agent{
                 docker {
-                  //get image from the registry
-                  /*
-                  image "${params.DOCKER_IMAGE}"
-                  registryUrl "${params.DOCKER_REG_ARTIFACTORY}"
-                  registryCredentialsId "${params.DOCKER_REG_ARTIFACTORY_TOKEN}"
-                  */
                   image DOCKER_IMAGE
                   registryUrl DOCKER_REG_ARTIFACTORY
                   registryCredentialsId DOCKER_REG_ARTIFACTORY_TOKEN
@@ -44,31 +31,29 @@ def call(body) {
               }
               steps {
                 //Link can't be literally here #########
+
+                //testing
+                echo 'Testing docker run from artifactory'
+                /*
                 sh"""
                   echo Cloning Repo
                   git clone https://github.com/airfcms/hello_world.git
-                 """
-             }
-            }
-          }
-          //Not the stage Várzea asked stage('unit testing')
-          //For testing purposes
-          stage('compile'){
-                //Call cmake command to compile the CMakeLits.txt file inside the cloned repo
-                //Repository name can't be literal ###########
-                sh"""
                   mkdir hello_world/build
                   cd hello_world/build
                   cmake ..
                   make
                   ./hello_world
-                """
+                 """
+                 */
+             }
+            }
           }
+
           //Not the stage Várzea asked stage(???)
           //For testing purposes
           //Push the artifact to Azure Artifactory Generic registry
           stage('deploy') {
-            
+
           }
         }
 }
