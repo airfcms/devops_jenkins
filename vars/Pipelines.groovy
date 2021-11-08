2bff7c51a1b6aee6a796e0f8d1183c0ee9e0267c
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-08 14:10:51 +0000

    change and improvement to be more readable

--- vars/Pipelines.groovy
+++ vars/Pipelines.groovy
@@ -1,14 +1,8 @@
-//Cleaning is needed and Integrate with SonnarQube
+//Cleaning is needed and Integration with SonnarQube
 
 def call(body) {
     // evaluate the body block, and collect configuration into the objectdef
     pipelineParams = [:]
-
-    //Appears with null value.... why?!
-    DOCKER_IMAGE = pipelineParams['dockerImage']
-    DOCKER_REG_ARTIFACTORY = pipelineParams['dockerRegistryUrl']
-    DOCKER_REG_ARTIFACTORY_TOKEN = pipelineParams['dockerRegistryUrl']
-    scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
 
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipelineParams
@@ -16,89 +10,77 @@ def call(body) {
 
     pipeline {
           environment{
-
-            DOCKER_IMAGE = """
-                            ${sh(returnStdout: true, script: "echo ${pipelineParams['dockerImage']}")}
-                          """ //For some reason this is null but it echo's the right string.. nut it doesn't show an error anymore.. why?!
-             /*
-            DOCKER_REG_ARTIFACTORY = pipelineParams['dockerRegistryUrl']
-            DOCKER_REG_ARTIFACTORY_TOKEN = pipelineParams['dockerRegistryUrl']
-            scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
-            */
-            DOCKER_CREDENTIALS_ID = credentials('JFrog_Artifactory_Docker_Registry')
-
+            REPOSITORY_NAME= pipelineParams['repositoryName']
+            BUILD_DIRECTORY= pipelineParams['cmakeBuildDir']
             }
 
         agent any
           stages {
             stage('build') {
               agent{
+                environment{
+                  DOCKER_IMAGE = pipelineParams['dockerImage']
+                  DOCKER_REG_ARTIFACTORY = pipelineParams['dockerRegistryUrl']
+                  SCM_URL = scm.getUserRemoteConfigs()[0].getUrl()
+                }
                 docker {
-                  image 'csw-docker-registry/csw-airfcms-ubuntu'
-                  registryUrl 'https://airfcms.jfrog.io/'
-                  //registryCredentialsId 'docker-registry' //Not need because I changed the configuration system credentials
+                  reuseNode true //Don't see the difference on::off ### From the consoleOutput it seems the image is removed when the building stage is finished. Need to check why!!! <---------------
 
-                  reuseNode true //Don't see the difference on::off
+                  image env.DOCKER_IMAGE
+                  registryUrl env.DOCKER_REG_ARTIFACTORY
                 }
               }
               steps {
                 //Link can't be literally here #########
-                sh 'env' //To check available global variables
-                sh 'rm -rf ${WORKSPACE}/*' //Work around because of deleteDir() and cleanWS() jenkins bugs
+                sh 'env | sort' //To check available global variables
 
                 sh"""
-                  echo Cloning Repo
-                  git clone ${scmUrl}
-                  mkdir hello_world/build
-                  cd hello_world/build
-                  cmake ..
-                  make
-                  ./hello_world
+                  echo Cloning Repository in Docker Image Workspace
+                  git clone ${env.SCM_URL}
+                  cmake -S ${env.REPOSITORY_NAME} -B ${env.BUILD_DIRECTORY}
+                  make -C ${env.BUILD_DIRECTORY}
+                  ./${env.BUILD_DIRECTORY}/${env.REPOSITORY_NAME}
                  """
              }
             }
             //Not the stage Várzea asked stage(???)
             //For testing purposes
             //Push the artifact to Azure Artifactory Generic registry
-            stage('deploy') { //it seems that the docker image is remove before this stage !!IMPORTANT!!
+            stage('deploy') { //It seems that the docker image is remove before this stage !!IMPORTANT!!
+              environment{
+                GENERIC_REGISTRY_ID = pipelineParams['artifactoryGenericRegistry_ID']
+                REGISTRY_MAIN_DIRECTORY = 'build-repo'
+                ARTIFACT_NAME = env.REPOSITORY_NAME
+              }
               steps{
 
-                sh"""
-                  ls -lha
-                  echo ############################################################################
-                  echo ############################################################################
-                  ls -lha ..
-                  pwd
-                """
-               /* rtServer ( //Not need because i changed the credentials in configuration system for user:pass
-                    id: 'artifactory_generic_repository',
-                    url: 'http://40.67.228.51:8082/artifactory',
-                    // If you're using Credentials ID:
-                    credentialsId: 'artifact_registry'
-                )
-            */
-
                 rtUpload(
-                      serverId: 'artifactory_generic_repository',
+                      serverId: env.GENERIC_REGISTRY_ID,
                       spec: '''{
                                 "files": [
                                            {
-                                            "pattern": "*/hello_world",
-                                            "target": "build-repo/"
+                                            "pattern": "*/${env.ARTIFACT_NAME}",
+                                            "target": "${env.REGISTRY_MAIN_DIRECTORY}/"
                                             }
                                          ]
                                 }'''
                 )
 
-                rtPublishBuildInfo ( //Just to not get the notification error
-                    serverId: 'artifactory_generic_repository'
+                rtPublishBuildInfo ( //Send notification and prevents an error.
+                    serverId: env.GENERIC_REGISTRY_ID
                 )
               }
-            }
-          }
+            } //stage(deploy) closed bracket
+          } //stages body closed bracket
+          post {
+              always{
+                sh 'echo In post block -> Clean Workspace'
+                clean_workspace_WorkAround(env.WORKSPACE)
+              }
+          }//post body closed bracket
+        } //pipeline body closed bracket
+} //def body closed bracket
 
-
-        }
+def clean_workspace_WorkAround(String workspace){
+    sh 'rm -rf ${workspace}/*' //Work around because the declarative sintax bugs with deleteDir() and cleanWS()
 }
-
-
