aff4579ee97ae2069a06f6baeddeb1d91e375e5c
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-09 16:51:19 +0000

    varzea cenas jfrog

--- vars/Pipelines.groovy
+++ vars/Pipelines.groovy
@@ -28,7 +28,7 @@ def call(body) {
 
                 //Work around because the declarative sintax bugs with deleteDir() and cleanWS()
                 sh 'rm -rf ${WORKSPACE}/*'
-
+                echo scmUrl
                 sh"""
                   echo Cloning Repository in Docker Image Workspace
                   git clone ${scmUrl}
