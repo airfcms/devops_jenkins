42a9b323c02663fbf3d82981f82296da2090a46d
mavarzea <mavarzea@criticalsoftware.com>
2021-11-09 14:26:09 +0000

    Changed workspace

--- vars/Pipelines.groovy
+++ vars/Pipelines.groovy
@@ -74,7 +74,7 @@ def call(body) {
                 steps {
                   withSonarQubeEnv('sonarqube_airfcms') {
                     //-X is enabled to get more information in console output (jenkins)
-                    sh "cd ${WORKSPACE}; ${scannerHome}/bin/sonar-scanner -X -Dproject.settings=sonar-project.properties"
+                    sh "cd ${WORKSPACE}/${pipelineParams['repositoryName']}; ${scannerHome}/bin/sonar-scanner -X -Dproject.settings=sonar-project.properties"
                   }
                   timeout(time: 5, unit: 'MINUTES') {
                     waitForQualityGate abortPipeline: true
