32f20a76ea568f5df22b0b3301be05f44ada5dc6
mavarzea <mavarzea@criticalsoftware.com>
2021-11-21 18:41:40 +0000

    Fixed bug

--- vars/Pipelines.groovy
+++ vars/Pipelines.groovy
@@ -5,6 +5,7 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipelineParams
     body()
-
-   GenericPipeline pipelineParams    
+   echo "HERE"
+   GenericPipeline pipelineParams
+   echo "HERE1"    
 }
