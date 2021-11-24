826dfe815bc601af7c47b96bdf64812753c5a555
Ricardo Rosa <ricardorosa.fa@gmail.com>
2021-11-24 09:09:19 +0000

    regex griding

--- vars/GenericPipeline.groovy
+++ vars/GenericPipeline.groovy
@@ -93,15 +93,13 @@ def call(Map pipelineParams) {
             } //stage(static analysis) closed bracket
             stage('deploy') {
               steps{
-                  publishChecks name: 'Deployment',
-                                text: 'testing -> manual status: in progress',
-                                status: 'IN_PROGRESS'
-                  rtServer (
+
+                 rtServer (
                     id: pipelineParams['artifactoryGenericRegistry_ID'],
                     url: 'http://40.67.228.51:8082/artifactory',
                     credentialsId: 'artifact_registry'
-                  )
-                  rtUpload(
+                )
+                rtUpload(
                       serverId: pipelineParams['artifactoryGenericRegistry_ID'],
                       spec: """{
                                 "files": [
@@ -111,28 +109,30 @@ def call(Map pipelineParams) {
                                             }
                                          ]
                                 }"""
-                  )
-                  rtPublishBuildInfo (
+                )
+                rtPublishBuildInfo (
                     serverId: pipelineParams['artifactoryGenericRegistry_ID']
-                  )
+                )
 
              script {
-  		                artifactoryRegexLink_Pattern = /^Build\ssuccessfully\sdeployed.\sBrowse\sit\sin\sArtifactory\sunder\s(.*)$/
+  		                def artifactoryRegexLink_Pattern = /^Build\ssuccessfully\sdeployed.\sBrowse\sit\sin\sArtifactory\sunder\s(.*)$/
+		                  def matcher = null
 
-                      for(String line in currentBuild.getRawBuild().getLog(10)){
+                     for(String line in currentBuild.getRawBuild().getLog(10)){
 
                   			matcher = line =~ artifactoryRegexLink_Pattern
+
                   			if (matcher.matches())
                   			{
+                  			  println matcher[0][1]
                   			  artifactoryLink = matcher[0][1]
+                  			  println artifactoryLink
                   			}
-                      }
+
+                     }
               }
 
-			  	      publishChecks name: 'Deployment'/*,
-                              text: 'To view the artifact please access it clicking the link below',
-                              status: 'COMPLETED',
-                              detailsURL: artifactoryLink*/
+			  	      publishChecks name: 'Deployment'
               }
             } //stage(deploy) closed bracket
           } //stages body closed bracket
