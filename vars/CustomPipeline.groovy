0010b366e9d1fbb8312bed650f38664350cc2c56
Tiago Martins <tiago.j.martins@criticalsoftware.com>
2022-01-07 18:12:25 +0000

    changed agent

--- vars/CustomPipeline.groovy
+++ vars/CustomPipeline.groovy
@@ -95,6 +95,14 @@ def call(Map pipelineParams) {
 			  }
             }
             stage('static analysis') {
+              agent{
+                docker {
+                  image pipelineParams['dockerImage']
+                  registryUrl pipelineParams['dockerRegistryUrl']
+                  registryCredentialsId 'docker-registry'
+                  reuseNode true
+                }
+          }
                 environment {
                   scannerHome = tool 'sonnar_scanner'
                 }
