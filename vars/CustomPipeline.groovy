31bcd2d18cf11a1c8a67bce6c6d8f1b1679b3839
Tiago Martins <tiago.j.martins@criticalsoftware.com>
2022-01-07 16:40:19 +0000

    debug

--- vars/CustomPipeline.groovy
+++ vars/CustomPipeline.groovy
@@ -97,7 +97,6 @@ def call(Map pipelineParams) {
                     sh"""
                     cd ${pipelineParams['cmakeBuildDir']}/tests
                     ctest -R "codeCoverage|cppcheckAnalysis"
-                    ls -la /opt/jenkins/small-agent/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonnar_scanner/bin/sonar-scanner
                     """
 
                     //cobertura to publish the reports
