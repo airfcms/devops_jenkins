fd4d35b0a0583ba3c62e535ea8feded4326f0edb
Tiago Martins <tiago.j.martins@criticalsoftware.com>
2022-01-07 11:50:12 +0000

    enter build

--- vars/CustomPipeline.groovy
+++ vars/CustomPipeline.groovy
@@ -74,8 +74,8 @@ def call(Map pipelineParams) {
                 ls -la
                 ls -la ..
                 pwd
+                junit skipPublishingChecks: true, testResults: 'gtest-report.xml'
                 """
-                junit skipPublishingChecks: true, testResults: "./${pipelineParams['cmakeBuildDir']}/gtest-report.xml"
                 //junit skipPublishingChecks: true, testResults: 'valgrind-report.xml'
 
                 }
