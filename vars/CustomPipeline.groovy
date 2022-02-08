022b19d0018341448882504b6cebe58be21e2600
tjmartinscsw <tiago.j.martins@criticalsoftware.com>
2022-02-08 18:01:08 +0000

    started support for generic webhook trigger

--- vars/CustomPipeline.groovy
+++ vars/CustomPipeline.groovy
@@ -36,10 +36,7 @@ def call(Map pipelineParams) {
                   printContributedVariables: true,
                   printPostContent: true,
 
-                  silentResponse: false,
-
-                  regexpFilterText: '$Priority',
-                  regexpFilterExpression: 'Medium'
+                  silentResponse: false
                   )
                 }
           stages {
