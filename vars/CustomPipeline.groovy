94196b96b0b7d4edb9c81347906f9b9742907b0c
tjmartinscsw <tiago.j.martins@criticalsoftware.com>
2022-02-15 21:51:12 +0000

    checking if the build exists4

--- vars/CustomPipeline.groovy
+++ vars/CustomPipeline.groovy
@@ -462,15 +462,14 @@ def call(Map pipelineParams) {
                   issueKey: "${env.ISSUE_KEY}",
                   body: "Build [${env.BUILD_DISPLAY_NAME}|${env.BUILD_URL}] succeded!"
                 )
-              step([$class: 'IssueFieldUpdateStep', issueSelector: [$class: 'JqlIssueSelector', jql: "issuekey = ${env.ISSUE_KEY}"], fieldId: '10801', fieldValue:  "Deployed" ]);
-              step([$class: 'JiraIssueUpdateBuilder', jqlSearch: "issuekey = ${env.ISSUE_KEY}", workflowActionName: 'To Testing', comment:  "Deployed!!" ]);
+              step([$class: 'IssueFieldUpdateStep', issueSelector: [$class: 'JqlIssueSelector', jql: "issuekey = ${env.ISSUE_KEY}"], fieldId: '10900', fieldValue:  "Deployed" ]);
             }
             failure {
               jiraComment(
                   issueKey: "${env.ISSUE_KEY}",
                   body: "Build [${env.BUILD_DISPLAY_NAME}|${env.BUILD_URL}] failed!"
                 )
-              step([$class: 'IssueFieldUpdateStep', issueSelector: [$class: 'JqlIssueSelector', jql: "issuekey = ${env.ISSUE_KEY}"], fieldId: '10801', fieldValue:  "Deployment Failed" ]);
+              step([$class: 'JiraIssueUpdateBuilder', jqlSearch: "issuekey = ${env.ISSUE_KEY}", workflowActionName: 'To Testing', comment:  "Build [${env.BUILD_DISPLAY_NAME}|${env.BUILD_URL}] has FAILED!" ]);
 
               //step([$class: 'IssueFieldUpdateStep', issueSelector: [$class: 'jql'], fieldId: 'status', fieldValue:  "Deployment Failed" ]);
             }
