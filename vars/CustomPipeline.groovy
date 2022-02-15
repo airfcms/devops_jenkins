2f9a4f05333f3815db7c52b23e4b6efdf52e4833
tjmartinscsw <tiago.j.martins@criticalsoftware.com>
2022-02-15 19:07:59 +0000

    checking if the build exists4

--- vars/CustomPipeline.groovy
+++ vars/CustomPipeline.groovy
@@ -462,7 +462,7 @@ def call(Map pipelineParams) {
                   issueKey: "${env.ISSUE_KEY}",
                   body: "Build [${env.BUILD_DISPLAY_NAME}|${env.BUILD_URL}] succeded!"
                 )
-              step([$class: 'IssueFieldUpdateStep', issueSelector: [$class: 'jql'], fieldId: '10801', fieldValue:  "Deployed" ]);
+              step([$class: 'IssueFieldUpdateStep', issueSelector: [$class: 'JqlIssueSelector', jql: "issuekey = ${env.ISSUE_KEY}"], fieldId: '10801', fieldValue:  "Deployment Failed" ]);
             }
             failure {
               jiraComment(
