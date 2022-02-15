44678870ccc01541b0cc22e0f9a5e036a0957cbb
tjmartinscsw <tiago.j.martins@criticalsoftware.com>
2022-02-15 17:41:30 +0000

    checking if the build exists4

--- vars/CustomPipeline.groovy
+++ vars/CustomPipeline.groovy
@@ -445,14 +445,14 @@ def call(Map pipelineParams) {
                                     detailsURL: artifactoryLink
               }
             } //stage(promote) closed bracket
-            stage(Post){
+            stage(post){
               when { expression { issueKey } }
               Steps {
                 jiraComment:
                   issueKey: issueKey
                   body: "Success!!!"
               }
-            }
+            } //stage(post) closed bracket
           } //stages body closed bracket
         } //pipeline body closed bracket
 } //def body closed bracket
