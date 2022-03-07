2359fe3f6d69762fea55c64ef1c126dce143dbe8
tjmartinscsw <tiago.j.martins@criticalsoftware.com>
2022-03-07 11:39:13 +0000

    added credentials to git push

--- vars/CustomPipeline.groovy
+++ vars/CustomPipeline.groovy
@@ -37,13 +37,13 @@ def call(Map pipelineParams) {
                   GenericTrigger(
                     genericVariables: [
                       [key: 'issueKey', value: '$.issue.key'],
-                      [key: 'fixVersions', value: '$.issue.fields.fixVersions[0].name'],
+                      [key: 'fixVersions', value: '$.issue.fields.fixVersions[0].name', defaultValue: '0'],
                       [key: 'buildID', value: '$.issue.fields.customfield_10700', defaultValue: '0'], //defined default value so it does not fail
                       [key: 'deployment', value: '$.issue.fields.status.name'],
                       [key: 'changelogStatus', value: '$.changelog.items[0].field'], //if status we use the below ones
                       [key: 'fromWorkflow', value: '$.changelog.items[0].fromString'],
                       [key: 'deploymentStatus', value: '$.issue.fields.customfield_11100'],
-                      [key: 'releaseVersion', value: '$.version.name'], //From here, parameters related to release
+                      [key: 'releaseVersion', value: '$.version.name', defaultValue: '0'], //From here, parameters related to release
                       [key: 'released', value: '$.version.released'], //With this we evaluate if the pipeline is to run
                       [key: 'projectID', value: '$.version.projectId'], //Need this so we can create an issue if necessary
                       [key: 'releaseVersionID', value: '$.version.id']
@@ -81,9 +81,9 @@ def call(Map pipelineParams) {
                 // If not, it will be calculated
                 script{
                   try{
-                    if (fixVersions){
+                    if (fixVersions != "0"){
                       env.FIX_VERSIONS = fixVersions //passed the trigger and was defined in the Jira issue
-                    } else if (releaseVersion){
+                    } else if (releaseVersion != "0"){
                       env.FIX_VERSIONS = releaseVersion //passed the trigger and was defined in the Jira Release
                     } else {
                       println(">>> Fix/Release version not defined! Might be triggered manually or by commit. Going to get it from the Branch name.")
